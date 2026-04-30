2badafb79e0b7c466d740aa59a30f9f227b80fd7
Robyn McLaughlin <mclau448@d.umn.edu>
2026-04-30 15:00:50 -0500

    Initial setup of the graphics page

---
+++ lib/view/graphics_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class GraphicsPageScreen extends StatefulWidget {
+  const GraphicsPageScreen({super.key});
+
+  @override
+  State<GraphicsPageScreen> createState() => _GraphicsPageScreenState();
+}
+
+class _GraphicsPageScreenState extends State<GraphicsPageScreen> {
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
