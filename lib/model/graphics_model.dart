42cab1d82dd1467b4466aaa30804d9be361eeaf6
Robyn McLaughlin <mclau448@d.umn.edu>
2026-05-02 10:42:12 -0500

    Fetch data in the model.

---
+++ lib/model/graphics_model.dart
@@ -0,0 +1,13 @@
+import 'package:finalexam_salaries/model/ai_career_search_model.dart';
+
+import 'ai_job_data.dart';
+
+//This is a pretty sparse file, mostly it just exists to load the ai job data. However, I wanted to keep it to try to adhere to MVP as much as possible
+
+class GraphicsModel {
+  GraphicsModel();
+
+  Future <List<AIJob>> getJobData() async {
+    return await AIJobData.instance.loadJobs();
+  }
+}
