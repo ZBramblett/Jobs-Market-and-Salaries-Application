bcd62079b2384ce0043e08cc9ce7bd424ede1807
Robyn McLaughlin <mclau448@d.umn.edu>
2026-05-02 21:58:53 -0500

    Add button to navigate to analytics page in home screen app bar

--- lib/view/homepage.dart
+++ lib/view/homepage.dart
@@ -1,18 +1,10 @@
+import 'package:finalexam_salaries/view/graphics_page.dart';
 import 'package:flutter/material.dart';
 import 'package:finalexam_salaries/view/UI_functions.dart';
 import '../presenter/auth_presenter.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -35,6 +27,16 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
+        actions: [
+          IconButton(
+            onPressed: () {
+              Navigator.push(
+                context, 
+                MaterialPageRoute(builder: (_) => const GraphicsPageScreen()));
+            }, 
+            tooltip: 'Analytics',
+            icon: const Icon(Icons.analytics))
+        ],
       ),
       body: Center(
         child: Column(
