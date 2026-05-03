726a6a9f126a0b62e43b0b987dbc2f28d548b6f0
Derrick Johnson <joh22737@d.umn.edu>
2026-05-03 12:14:00 -0500

    convert search page to stateful widget with a  input field

--- lib/view/searchpage.dart
+++ lib/view/searchpage.dart
@@ -1,13 +1,40 @@
 import 'package:flutter/material.dart';
 
-class SearchPage extends StatelessWidget {
+class SearchPage extends StatefulWidget {
   const SearchPage({super.key});
+
+  @override
+  State<SearchPage> createState() => _SearchPageState();
+}
+
+class _SearchPageState extends State<SearchPage> {
+  final TextEditingController _searchController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Search')),
-      body: const Center(child: Text('Search')),
+      appBar: AppBar(title: const Text('Job Search')),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: _searchController,
+              decoration: const InputDecoration(
+                labelText: 'Enter job title',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            const SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: () {
+                // we will add search logic here next
+              },
+              child: const Text('Search'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
